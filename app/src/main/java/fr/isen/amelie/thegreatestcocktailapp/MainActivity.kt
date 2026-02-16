aa5daaaff4eb9f777079a299ad1e231d5988e367
AmelieDantonio <amelie.d-antonio-gallet@isen.yncrea.fr>
2026-02-16 11:58:18 +0100

    16/02 12h

--- app/src/main/java/fr/isen/amelie/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/fr/isen/amelie/thegreatestcocktailapp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fr.isen.amelie.thegreatestcocktailapp.ui.theme.TheGreatestCocktailAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,18 +42,55 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MonImage() {
+    Image(
+        painter = painterResource(id = R.drawable.image_fruits),
+        contentDescription = "Les Fruits",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
     )
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    TheGreatestCocktailAppTheme {
-        Greeting("Android")
+fun MonBouton() {
+    Button(onClick = {
+        println("Le bouton a été cliqué!")
+    }) {
+        Text("Mon Bouton")
     }
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Row {
+            Text("first !")
+            Text(
+                text = " Hello $name!",
+                color = Color.Red,
+                fontSize = 28.sp
+            )
+        }
+
+        Text("Coucou")
+        Row {
+            Text("Ligne 3")
+            Text(
+                text = " Partie 2",
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Right,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        MonImage()
+        MonBouton()
+    }
+
+}
+
