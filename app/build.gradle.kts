538dc1b5dcf442c529e3e8939bc6b47e34198a7e
AmelieDantonio <amelie.d-antonio-gallet@isen.yncrea.fr>
2026-02-17 16:09:50 +0100

    17/02 16h corrigé

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 }
